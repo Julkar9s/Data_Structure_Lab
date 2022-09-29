07afa23cc59a46fa783939916d73616118ff107a
Julkar9s <102204198+Julkar9s@users.noreply.github.com>
2022-09-29 21:45:52 +0600

    Update Lab2b_22-46705-1.cpp

--- Lab2_22-46705-1/Lab2b_22-46705-1.cpp
+++ Lab2_22-46705-1/Lab2b_22-46705-1.cpp
@@ -3,23 +3,36 @@ using namespace std;
 
 int main()
 {
-    int i,j;
-    int arr1[5] = {2,3,4,5,6};
-    int arr2[6] = {1,3,4,7,8,9};
-    int arr3[11];
-    for(int i = 0; i<5; i++){
-        arr3[i] = arr1[i];
-    }
+    int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+   int arr2[10] = {5,6,7,8,9,10,11,12,13,14};
+   int arr3[10], i, j, k=0, l, flag;
 
-    for(int i = 0; i<6; i++){
-        arr3[i] = arr2[i];
-    }
+   for(i=0; i<10; i++)
+   {
+      for(j=0; j<10; j++)
+      {
+         if(arr1[i]==arr2[j])
+         {
+            flag = 0;
+            for(l=0; l<k; l++)
+            {
+               if(arr1[i]==arr3[l])
+                  flag++;
+            }
+            if(flag==0)
+            {
+               arr3[k] = arr1[i];
+               k++;
+            }
+         }
+      }
+   }
 
+   for(i=0; i<k; i++)
+      cout<<arr3[i]<<" ";
 
-    for( int i = 0; i<5; i++){
-        if(arr1[i] == arr2[i])
-            cout<<arr3[i]<<" ";
-    }
+   cout<<endl;
+   return 0;
 
 
 
